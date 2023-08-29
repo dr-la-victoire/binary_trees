0e00e1272b0ca80b051ea0e1cc56f83887bd982f
la victoire <lavictoire633@gmail.com>
2023-08-29 00:54:19 -0700

    Correction on Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,27 +12,22 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	/* checking if parent has a value */
+	newNode = binary_tree_node(parent, value);
+	/* checking if parent is empty */
 	if (parent == NULL)
-		return (NULL);
-	/* creating the left node */
-	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->n = value;
-	newNode->right = NULL;
-	newNode->parent = parent;
-	/* checking if parent has no child */
-	if (!parent->left)
 	{
-		newNode->left = NULL;
-		parent->left = newNode;
+		free(newNode);
+		return (NULL);
 	}
-	else
+
+	/* checking if parent->left is not empty */
+	if (parent->left)
 	{
 		newNode->left = parent->left;
 		parent->left->parent = newNode;
 	}
+	/* assigning parent->left to newNode */
+	parent->left = newNode;
 
 	return (newNode);
 }
